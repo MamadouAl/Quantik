0bda28fc7ac954a6c934613de0b31e7be9d9a787
dm213333 <dm213333@etu.univ-lehavre.fr>
2024-01-19 15:27:15 +0100

    Debut d'implementation de la class 'ArrayPieceQuantik'

---
+++ PHP/ArrayPieceQuantik.php
@@ -0,0 +1,44 @@
+<?php
+
+class ArrayPieceQuantik implements ArrayAccess, Countable
+{
+    // #pieceQuantik : PieceQuantik[]
+    protected $pieceQuantik;
+
+    public function __construct()
+    {
+        $this->pieceQuantik = new PieceQuantik();
+    }
+
+    public function getPieceQuantik(int $position)
+    {
+        return new pieceQuantik[$position];
+    }
+
+    public function setPieceQuantik(int $position, PieceQuantik $pieceQuantik)
+    {
+        $this->pieceQuantik[$position] = $pieceQuantik;
+    }
+
+    public function addPieceQuantik(PieceQuantik $pieceQuantik)
+    {
+        $this->pieceQuantik[] = $pieceQuantik;
+    }
+
+    public function removePieceQuantik(int $position)
+    {
+        unset($this->pieceQuantik[$position]);
+    }
+
+    // Génère les tableaux de 8 pièces de chaque couleur nécessaires au jeu
+    public static function initPieceQuantikNoires(): ArrayPieceQuantik
+    {
+        $arrayPieceQuantik = new ArrayPieceQuantik();
+
+        for ($i = 0; $i < 8; $i++) {
+            $arrayPieceQuantik->addPieceQuantik(new PieceQuantik("noir"));
+        }
+
+        return $arrayPieceQuantik;
+    }
+}
