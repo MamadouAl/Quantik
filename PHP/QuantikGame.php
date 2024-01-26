82e7cf9c2caea30e7b376468530479c5967788e3
mouhamed lamine kebe <mouhamed-lamine.kebe@etu.univ-lehavre.fr>
2024-01-26 16:01:18 +0100

    ajout de la class QuantikGame

---
+++ PHP/QuantikGame.php
@@ -0,0 +1,10 @@
+<?php
+
+class QuantikGame extends AbstractGame
+{
+    public PlateauQuantik $plateau;
+    public  ArrayPieceQuantik $pieceBlack;
+    public ArrayPieceQuantik $pieceWhite;
+    public array $couleurPlayer;
+
+}
