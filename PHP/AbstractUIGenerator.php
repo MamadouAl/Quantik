df818e7618781d8b7ffe07073c5ec8e51b121f2c
dm213333 <dm213333@etu.univ-lehavre.fr>
2024-01-26 16:05:52 +0100

    Ajout de la classe 'AbstractUIGenerator'

---
+++ PHP/AbstractUIGenerator.php
@@ -0,0 +1,48 @@
+<?php
+
+class AbstractUIGenerator
+{
+    /**
+     * Methode permettant de générer le début d'un fichier HTML
+     * @param string $title
+     * @return string
+     */
+    public static function getDebutHTML(string $title): string
+    {
+        return "<!DOCTYPE html>
+        <html lang=\"fr\">
+        <head>
+            <meta charset=\"UTF-8\">
+            <title>$title</title>
+            <link rel=\"stylesheet\" href=\"style.css\">
+        </head>
+        <body>";
+    }
+
+    /**
+     * Methode permettant de générer la fin d'un fichier HTML
+     * @return string
+     */
+    public static function getFinHTML(): string
+    {
+        return "</body>
+        </html>";
+    }
+
+    /**
+     * Methode permettant générer une page d'erreur
+     * @param string $message
+     * @param string $urlLien
+     * @return string
+     */
+    public static function getPageErreur(string $message, string $urlLien ) : string {
+        return self::getDebutHTML("Page d'erreur") .
+            "<div>
+                <h1>Erreur</h1>
+                <p>$message</p>
+                <a href=\"$urlLien\">Retour</a>
+            </div>" .
+            self::getFinHTML();
+    }
+
+}
